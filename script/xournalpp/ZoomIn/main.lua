49bd3fb87e22976597f2099d9cdd2dea26875a1f
iacobucci <iacobuccivalerio@gmail.com>
2022-05-31 17:22:57 +0200

    deleted:    script/bspwm/rofi-launcher 	modified:   script/polybar/bspwm-bar 	deleted:    script/xournalpp/ZoomIn/main.lua 	deleted:    script/xournalpp/ZoomIn/plugin.ini 	new file:   share/applications/cmake-gui.desktop 	new file:   share/applications/ipython.desktop 	new file:   share/applications/jupyter-notebook.desktop

--- script/xournalpp/ZoomIn/main.lua
+++
@@ -1,9 +0,0 @@
--- Register all Toolbar actions and intialize all UI stuff
-function initUi()
-  app.registerUi({["menu"] = "Zoom in", ["callback"] = "zoomin", ["accelerator"] = "="});
-end
-
-function cycle()
-function zoomin()
-	app.uiAction({["action"] = "ACTION_ZOOM_IN"})
-end
